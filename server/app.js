60f969eeaaf26d431fee1de54f2c19083088b053
john katua <johnkatua99@gmail.com>
2021-05-13 11:01:56 +0300

    added mongoose which will help bulid and query our database in mongodb

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+// app
 const app = express();
+
+// db
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+  useCreateIndex: true
+}).then(() => {
+  console.log('mongodb connected');
+})
 
 app.get('/', (req, res) => {
   res.send('Hello from node');
