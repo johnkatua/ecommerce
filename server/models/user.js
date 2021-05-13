fa7e25e15803ed858dc82933d63212383040b50c
john katua <johnkatua99@gmail.com>
2021-05-13 12:09:24 +0300

    added virtual field to encrypt password provided by the app user once he logs in

--- server/models/user.js
+++ server/models/user.js
@@ -36,4 +36,30 @@ const userSchema = new mongoose.Schema({
 {
   timestamps: true
 }
-)
+);
+
+// virtual field
+userSchema.virtual('password')
+  .set((password) => {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(() => {
+    return this._password;
+  });
+
+userSchema.methods = {
+  encryptPassword: (password) => {
+    if (!password) return '';
+    try {
+      return crypto.createHmac('sha1', this.salt)
+                    .update(password)
+                    .digest('hex');
+    } catch (error) {
+      return '';
+    }
+  }
+};
+
+module.exports = mongoose.model('User', userSchema);
