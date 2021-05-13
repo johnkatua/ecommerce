ea53aa87f5c7acc2d9f6fcb5c5a95c8617a34168
john katua <johnkatua99@gmail.com>
2021-05-13 11:26:43 +0300

    added a new folder to handle all the logic for our api

---
+++ server/controllers/user.js
@@ -0,0 +1,3 @@
+exports.welcomeUser = (req, res) => {
+  res.json({ message: "Welcome to the shopping center"});
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const {welcomeUser} = require('../controllers/user');
 
-router.get('/user', (req, res) => {
-  res.send('Hello am a new user');
-});
+router.get('/user', welcomeUser);
 
 module.exports = router;
