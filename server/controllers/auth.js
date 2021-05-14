061f094a01368d6a9fe5c730a38f3c4a031df644
john katua <johnkatua99@gmail.com>
2021-05-14 15:31:26 +0300

    added two middlewares one to check user is authenticated and other to make some resources are only accessible to admins

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -56,3 +56,23 @@ exports.requireSignin = expressJwt({
   userProperty: 'auth',
   algorithms: ['HS256']
 });
+
+exports.isAuth = (req, res, next) => {
+  let user = req.profile && req.auth && req.profile._id == req.auth._id;
+  
+  if (!user) {
+    return res.status(403).json({
+      err: 'Access denied'
+    })
+  };
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      err: 'Admin resource! Access denied'
+    });
+  };
+  next ();
+}
