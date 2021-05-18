2e32e11d20d7a5d885a8008047dede8b96864817
john katua <johnkatua99@gmail.com>
2021-05-18 13:56:46 +0300

    added a functionality to get only a single product through productId

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -4,6 +4,24 @@ const _ = require('lodash');
 const fs = require('fs');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: 'Product not found'
+      })
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  res.json(req.product);
+
+}
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtension = true;
@@ -13,18 +31,31 @@ exports.create = (req, res) => {
         err: 'Image could not be loaded'
       })
     }
+    // check for all fields
+    const {name, description, price, category, quantity, shipping} = fields
+    if (!name || !description || !price || !category || !quantity || !shipping) {
+      return res.status(400).json({
+        error: 'All fields required'
+      })
+    }
+
     let product = new Product(fields);
     if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1mb'
+        })
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
     product.save((err, result) => {
       if (err) {
         return res.status(400).json({
-          err: errorHandler(err)
+          error: errorHandler(err)
         })
       }
-      res.json({ result });
+      res.json( result );
     })
   })
-}
+};
