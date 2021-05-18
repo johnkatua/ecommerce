3878870079b957fbf87efd61b22b0558e7bbde83
john katua <johnkatua99@gmail.com>
2021-05-18 12:26:20 +0300

    added a functionality to create new product, I also formidable to help handle form data, I created route to enable user add new product

---
+++ server/controllers/product.js
@@ -0,0 +1,30 @@
+const Product = require('../models/product');
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
+const { errorHandler } = require('../helpers/dbErrorHandler');
+
+exports.create = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtension = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        err: 'Image could not be loaded'
+      })
+    }
+    let product = new Product(fields);
+    if (files.photo) {
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          err: errorHandler(err)
+        })
+      }
+      res.json({ result });
+    })
+  })
+}
