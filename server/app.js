4ad95f3a87fb7d43bcdb769be0eab257929be321
john katua <johnkatua99@gmail.com>
2021-05-19 13:40:41 +0300

    added functionality to enable user update his profile and able to view his credentials too, I installed cors to enables communication between backend and frontend

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator')
 require('dotenv').config();
@@ -30,6 +31,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // routes middleware
 app.use('/api', authRoutes);
