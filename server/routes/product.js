5d2cc99ce3b489cf3edc5dd96ec0d5c439d44100
john katua <johnkatua99@gmail.com>
2021-05-19 12:36:43 +0300

    added a functionality to enable user search product by given arguments

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {create, productById, read, remove, update, list, listRelated, listCategories} = require('../controllers/product');
+const {create, productById, read, remove, update, list, listRelated, listCategories, listBySearch} = require('../controllers/product');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
@@ -12,6 +12,7 @@ router.put('/product/:productId/:userId', requireSignin, isAdmin, isAuth, update
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
+router.post('/products/by/search', listBySearch);
 
 router.param('userId', userById);
 router.param('productId', productById);
