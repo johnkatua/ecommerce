3b683305a65f091433d9cc31769df7154b87a32c
john katua <johnkatua99@gmail.com>
2021-05-19 13:01:00 +0300

    added a functionality to display product photo

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -214,4 +214,12 @@ exports.listBySearch = (req, res) => {
         data
       })
     })
+};
+
+exports.photo = (req, res, next) => {
+  if (req.product.photo.data) {
+    res.set('Content-Type',  req.product.photo.contentType)
+    return res.send(req.product.photo.data)
+  };
+  next();
 }
