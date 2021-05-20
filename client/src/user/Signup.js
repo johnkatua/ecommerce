5071a3f5f00753c3c5b274cba326b9455435aa54
john katua <johnkatua99@gmail.com>
2021-05-20 14:48:09 +0300

    created a form to enable user to sign to the application

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { API } from '../config';
 import Layout from '../core/Layout';
 
 const Signup = () => {
+  const [values, setValues] = React.useState({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: false
+  });
+
+  const handleChange = name => event => {
+    setValues({...values, error: false, [name]: event.target.value})
+  }
+  const signUpForm = () => {
+    return (
+      <form>
+        <div className='form-group'>
+          <label className="text-muted">Name</label>
+          <input onChange={handleChange('name')} type='text' className='form-control' />
+        </div>
+        <div className='form-group'>
+          <label className="text-muted">Email</label>
+          <input onChange={handleChange('email')} type='email' className='form-control' />
+        </div>
+        <div className='form-group'>
+          <label className="text-muted">Password</label>
+          <input onChange={handleChange('password')} type='password' className='form-control' />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>    
+    )
+  }
   return (
-      <Layout title='Signup' description='Signup to Node React E-commerce app'>
-        {API}
+      <Layout title='Signup' description='Signup to Node React E-commerce app' className='container col-md-8 offset-md-2'>
+        {signUpForm()}
+        {JSON.stringify(values)}
       </Layout>
   )
 };
