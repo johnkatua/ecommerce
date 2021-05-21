3f2099e3544a3cd6c591e065072376f817a6939a
john katua <johnkatua99@gmail.com>
2021-05-21 15:39:07 +0300

    created a private route to ensure user logs in first before he/she has access to the dashboard

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,12 +3,15 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
+import PrivateRoute from './auth/PrivateRoute';
+import Dashboard from './user/UserDashboard';
 
 const Routes = () => {
   return(
       <BrowserRouter>
         <Switch>
           <Route path='/' exact component={Home} />
+          <PrivateRoute path='/dashboard' exact component={Dashboard} />
           <Route path='/signup' exact component={Signup} />
           <Route path='/signin' exact component={Signin} />
         </Switch>
---
+++ client/src/user/UserDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+const Dashboard = () => {
+  return (
+    <Layout title='Dashboard' description='User dashboard'>
+      ...
+    </Layout>
+  )
+};
+
+export default Dashboard;
