9e5441f3dcf8e03759e17c82ed5a357187b6b349
john katua <johnkatua99@gmail.com>
2021-05-21 11:30:26 +0300

    enable signup functionality from the frontend side

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../core/Layout';
+import {API} from '../config';
 
 const Signup = () => {
   const [values, setValues] = React.useState({
@@ -10,8 +11,33 @@ const Signup = () => {
     success: false
   });
 
+  const {name, email, password} = values;
+
   const handleChange = name => event => {
     setValues({...values, error: false, [name]: event.target.value})
+  };
+
+  const signup = (user) => {
+    // console.log(name, email, password);
+    fetch(`${API}/signup`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => {
+      return res.json();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    signup({name, email, password});
   }
   const signUpForm = () => {
     return (
@@ -28,7 +54,7 @@ const Signup = () => {
           <label className="text-muted">Password</label>
           <input onChange={handleChange('password')} type='password' className='form-control' />
         </div>
-        <button className="btn btn-primary">Submit</button>
+        <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
       </form>    
     )
   }
