e1c4cf3e8aa26d2cf6f8bfd5588bae9be7786d27
john katua <johnkatua99@gmail.com>
2021-05-26 12:36:11 +0300

    added function to sort out categories according to checked or unchecked

--- client/src/core/Checkbox.js
+++ client/src/core/Checkbox.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Checkbox = ({categories}) => {
+  const [checked, setChecked] = useState([]);
+
+  const handleToggle = category => () => {
+    // return 1st index or -1
+    const currentCategoryId = checked.indexOf(category);
+    const newCheckedCategoryId = [...checked];
+    // push the currently checked category if it was not checked before
+    // else take it off
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(category);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1)
+    }
+    console.log(newCheckedCategoryId);
+    setChecked(newCheckedCategoryId);
+  }
   return categories.map((category, idx) => {
     return <li className='list-unstyled' key={idx}>
-      <input type="checkbox" className="form-check-input" />
+      <input onChange={handleToggle(category._id)} value={checked.indexOf(category._id === -1)} type="checkbox" className="form-check-input" />
       <label className="form-check-label">{category.name}</label>
     </li>
   }) 
