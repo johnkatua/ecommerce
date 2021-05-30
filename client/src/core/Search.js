561d9d7ee5d43fd33b3aa226bbaead7078ccd2dd
john katua <johnkatua99@gmail.com>
2021-05-30 10:40:50 +0300

    created a search bar, with a dropdown to display all categories, an input field and a search button

--- client/src/core/Search.js
+++ client/src/core/Search.js
@@ -26,9 +26,44 @@ const Search = () => {
   useEffect(() => {
     loadCategories()
   }, []);
+
+  const searchSubmit = () => {
+
+  };
+
+  const handleChange = () => {
+
+  }
+
+  const searchForm = () => {
+    return (
+      <form onSubmit={searchSubmit}>
+        <span className="input-group-text">
+          <div className="input-group input-group-lg">
+            <div className="input-group-prepend">
+              <select className="btn">
+                <option value="All">Pick a category</option>
+                {categories.map((category, idx) => (
+                  <option value={category._id} key={idx}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <input type="search" className="form-control" onChange={handleChange('search')} placeholder='Search by name' />
+          </div>
+          <div className="btn input-group-append" style={{border: 'none'}}>
+            <button className="input-group-text">Search</button>
+          </div>
+        </span>
+      </form>
+    )
+  }
   return (
-    <div>
-      <h2>Search bar :{JSON.stringify(categories)}</h2>
+    <div className='row'>
+      <div className="container mb-3">
+        {searchForm()}
+      </div>
     </div>
   )
 };
