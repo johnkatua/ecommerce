82414dc19f9e94b265c3c4e78338c3db0dcbc979
john katua <johnkatua99@gmail.com>
2021-06-04 13:39:18 +0300

    added a method to add products tothe cart component

--- client/src/core/Card.js
+++ client/src/core/Card.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import {Link, Redirect} from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
+import {addItem} from './CartHelpers';
 
 const Card = ({product, showViewProductButton = true}) => {
+  const [redirect, setRedirect] = useState(false)
   const showViewButton = (showViewProductButton) => {
     return (
       showViewProductButton && (
@@ -16,9 +18,21 @@ const Card = ({product, showViewProductButton = true}) => {
     )
   };
 
+  const addToCart = () => {
+    addItem(product, () => {
+      setRedirect(true)
+    })
+  };
+
+  const redirectUser = (redirect) => {
+    if (redirect) {
+      return <Redirect to='/cart' />
+    }
+  }
+
   const showAddToCartButton = () => {
     return (
-      <button className="btn btn-outline-success mt-2 mb-2">
+      <button onClick={addToCart} className="btn btn-outline-success mt-2 mb-2">
         Add to cart
       </button>
     )
@@ -35,6 +49,7 @@ const Card = ({product, showViewProductButton = true}) => {
       <div className="card">
         <div className="card-header name">{product.name}</div>
           <div className="card-body">
+            {redirectUser(redirect)}
             <ShowImage item={product} url='product' />
               <p className='lead mt-2'>{product.description.substring(0, 100)}</p>
               <p className='product-price'>Ksh{product.price}</p>
---
+++ client/src/core/CartHelpers.js
@@ -0,0 +1,28 @@
+export const addItem = (product, next) => {
+  let cart = [];
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem('cart')) {
+      // JSON.parse() is used to convert json to object
+      cart = JSON.parse(localStorage.getItem('cart'))
+    }
+    cart.push({
+      ...product,
+      count: 1
+    });
+    // the method removes any duplicates
+    // an array is build using the new Set and it converted back to an array using Array.from method so that it can be re-mapped
+    // new Set only allow unique values in it, so you pass ids of each product
+    // If the loop tries to add the similar product it will be ignored
+    // The first map function returns an array of it and when we run it on the second map it returns the actual product from the cart
+    cart = Array.from(new Set(cart.map((p) => {
+      return p._id;
+    }))).map((id) => {
+      return cart.find((p) => {
+        return p._id === id;
+      })
+    });
+    // JSON.stringify() is used to convert object to json
+    localStorage.setItem('cart', JSON.stringify(cart));
+    next();
+  }
+}
