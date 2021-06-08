ccf36af02d6b5b9d0d558c6a3152e61da983ad6f
john katua <johnkatua99@gmail.com>
2021-06-08 12:19:51 +0300

    installed braintree package so that it can be integrated with the application to enable users make payment once they purchase products

---
+++ server/routes/braintree.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const {requireSignin, isAuth} = require('../controllers/auth');
+const { generateToken } = require('../controllers/braintree');
+const { userById } = require('../controllers/user');
+
+router.get('/braintree/getToken/:userId', requireSignin, isAuth, generateToken);
+
+router.param('userId', userById);
+
+module.exports = router;
