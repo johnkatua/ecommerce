e4553d721d797467cb8be8f8bbbb4b28f77919f0
john katua <johnkatua99@gmail.com>
2021-06-14 15:09:17 +0300

    user can successfully update information and the be redirected to the cart component

--- client/src/user/Profile.js
+++ client/src/user/Profile.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { getUser, updateUser, updateUserLocally } from './ApiUser';
+import { Redirect } from 'react-router-dom';
 
 const Profile = ({match}) => {
   // console.log(match);
@@ -29,13 +30,58 @@ const Profile = ({match}) => {
   };
 
   useEffect(() => {
-      init(match.params.userId)
-    
+      init(match.params.userId) 
   }, []);
+
+  const handleChange = (name) => e => {
+    setValues({...values, error: false, [name]: e.target.value})
+
+  }
+
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    updateUser(match.params.userId, token, {name, email, password})
+      .then(data => {
+        if(data.error) {
+          console.log(data.error)
+        } else {
+          updateUserLocally(data, () => {
+            setValues({...values, name: data.name, email: data.email, success: true})
+          })
+        }
+      })
+  };
+
+  const redirectUser = (success) => {
+    if(success) {
+      return <Redirect to='/cart' />
+    }
+  }
+
+  const profileUpdate = (name, email, password) => {
+    return (
+      <form style={{width: '50%'}}>
+        <div className="form-group">
+          <label className="text-muted">Name</label>
+          <input type="text" className="form-control" onChange={handleChange('name')} value={name} />
+        </div>
+        <div className="form-group">
+          <label className="text-muted">Email</label>
+          <input type="email" className="form-control" onChange={handleChange('email')} value={email} />
+        </div>
+        <div className="form-group">
+          <label className="text-muted">Password</label>
+          <input type="password" className="form-control" onChange={handleChange('password')} value={password} />
+        </div>
+        <button onClick={clickSubmit} className="btn btn-primary mt-2">Submit</button>
+      </form>
+    )
+  }
   return (
   <Layout title='Profile Update' description='Update your profile' className='container-fluid'>
     <h2>Profile</h2>
-    {JSON.stringify(values)}
+    {profileUpdate(name, email, password)}
+    {redirectUser(success)}
   </Layout>
   );
 };
