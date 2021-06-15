c38333a7d097a171a19e36ee5c395daeee972845
john katua <johnkatua99@gmail.com>
2021-06-15 09:16:38 +0300

    created methods to enable user manage all products

---
+++ client/src/admin/ManageProducts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+
+const ManageProducts = () => {
+  return (
+    <Layout title='Manage Products' description='Perform CRUD on products' className='container fluid'>
+      <div className="row">
+        <div>Manage Products</div>
+      </div>
+    </Layout>
+  )
+};
+
+export default ManageProducts;
